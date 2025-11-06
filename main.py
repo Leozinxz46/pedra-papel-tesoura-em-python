b4239e7f8736b5b82352299866f2aac1b1ce4d54
Leonardo Francisco Da Silva <leozinho24680@gmail.com>
2025-11-06 20:33:01 -0300

    dando um update no projeto e consetando alguns erros)

--- main.py
+++ main.py
@@ -1,29 +1,79 @@
 def pedra_papel_tesoura():
 
-    import random
+    while True:
+        try:
 
-    print("=== Pedra Papeal Tesoura ===")
 
-    opcoes = ["pedra", "papel", "tesoura"]
-    maquina = random.choice(opcoes)
+            import random
 
-    usuario = str(input("pedra, papel ou tesoura: ")).lower
+            print("=== Pedra Papeal Tesoura ===")
 
-    if usuario == maquina:
-        print(f"{usuario} X {maquina}")
-        print("Empate")
-    elif usuario == "pedra" and maquina == "tesoura":
-        print(f"{usuario} X {maquina}")
-        print("Parabens voce venceu")
-    elif usuario == "tesoura" and maquina == "papel":
-        Print(f"{usuario} X {maquina}")
-        print("Parabens Voce venceu")
-    elif usuario == "papel" and maquina == "pedra":
-        Print(f"{usuario} X {maquina}")
-        print("Parabens voce venceu")
-    else:
-        print(f"{usuario} X {maquina}")
-        print("Voce perdeu")
+            opcoes = ["pedra", "papel", "tesoura"]
+            maquina = random.choice(opcoes)
+
+            usuario = str(input("pedra, papel ou tesoura: "))
+
+            if usuario == maquina:
+                print(f"{usuario} X {maquina}")
+                print("Empate")
+                continuar = str(input("Deseja continuar (s/n): "))
+                if continuar == "s":
+                    continue
+                else:
+                    print("Programa encerrando")
+                    break
+
+
+            elif usuario == "pedra" and maquina == "tesoura":
+                print(f"{usuario} X {maquina}")
+                print("Parabens voce venceu")
+                continuar = str(input("Deseja continuar (s/n): "))
+                if continuar == "s":
+                    continue
+                else:
+                    print("Programa encerrando")
+                    break
+
+
+            elif usuario == "tesoura" and maquina == "papel":
+                Print(f"{usuario} X {maquina}")
+                print("Parabens Voce venceu")
+                continuar = str(input("Deseja continuar (s/n): "))
+                if continuar == "s":
+                    continue
+                else:
+                    print("Programa encerrando")
+                    break
+
+            elif usuario == "papel" and maquina == "pedra":
+                Print(f"{usuario} X {maquina}")
+                print("Parabens voce venceu")
+                continuar = str(input("Deseja continuar (s/n): "))
+                if continuar == "s":
+                    continue
+                else:
+                    print("Programa encerrando")
+                    break
+
+
+            else:
+                print(f"{usuario} X {maquina}")
+                print("Voce perdeu")
+                continuar = str(input("Deseja continuar (s/n): "))
+                if continuar == "s":
+                    continue
+                else:
+                    print("Programa encerrando")
+                    break
+
+        except ValueError:
+            print("Valor digitado incorreto!")
+            continuar = str(input("Deseja continuar (s/n): "))
+            if continuar == "s":
+                continue
+            else:
+                print("Programa encerrando")
+                break
 
 
 pedra_papel_tesoura()
